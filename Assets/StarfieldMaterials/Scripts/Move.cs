08f87041136f9086c8168e2f440bf0024de11643
dibbik <nickdibbik@yandex.ru>
2023-05-31 22:35:12 +0300

    Добавьте файлы проекта.

---
+++ Assets/StarfieldMaterials/Scripts/Move.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine; 
+using System.Collections;
+
+public class Move : MonoBehaviour
+{
+    float Target;
+	float speed = 0.01f;
+	Rigidbody rigidbody;
+	float horizontal;
+	float vertical;
+
+
+	void Start()
+	{
+		rigidbody = GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+        //Target += Time.deltaTime / 125;
+
+        //transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y, Target), 0.05f);
+
+		horizontal = Input.GetAxis("Horizontal");
+		vertical = Input.GetAxis("Vertical");
+
+		rigidbody.AddForce(((transform.right * horizontal) + (transform.forward * vertical)) * speed/Time.deltaTime);
+	}
+}
